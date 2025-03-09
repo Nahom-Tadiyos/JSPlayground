889c482fddb2045334b8842bf8152df080444d72
Nahom <8nahom8@gmail.com>
2025-03-08 20:33:34 -0600

    added a if statement that checks if user has a valid deposit amount

--- script.js
+++ script.js
@@ -11,7 +11,12 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-    depositAmount = prompt("Enter a deposit amount: ")
+    const depositAmount = prompt("Enter a deposit amount: ")
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+            console.log("Invalid deposit amount, try again")
+        }
 }
 
 deposit();
