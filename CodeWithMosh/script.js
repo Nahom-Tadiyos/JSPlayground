fce8de01e1a108248bf062e9e6ad031b1b249d84
Nahom <8nahom8@gmail.com>
2025-04-20 14:18:41 -0500

    added basics like functions, comments, outputs and types of scripts

---
+++ CodeWithMosh/script.js
@@ -0,0 +1,27 @@
+//On page script
+{/* <script type="text/javascript">
+</script> */}
+
+
+//External JS file
+{/* <script src="filename.js"></script> */}
+
+
+//Functions
+function addNumbers(a, b) {
+    return a + b; ;
+   }
+   x = addNumbers(1, 2);
+
+
+//Outputs
+console.log(a); // write to the browser
+document.write(a); // write to the HTML
+alert(a); // output in an alert
+confirm("Really?"); // yes/no dialog, returns it
+prompt("Your age?","0"); // input dialog.
+
+
+/* Multi line
+ comment */
+// One line comments
