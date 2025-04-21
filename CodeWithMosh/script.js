4859c578125361b504f39b0fc6d4c00221fe9f80
Nahom <8nahom8@gmail.com>
2025-04-20 22:29:55 -0500

    added if/esle and data types

--- CodeWithMosh/script.js
+++ CodeWithMosh/script.js
@@ -25,3 +25,19 @@ prompt("Your age?","0"); // input dialog.
 /* Multi line
  comment */
 // One line comments
+
+//If- Else
+if ((age >= 14) && (age < 19)) { // logical
+    status = "Eligible."; // execute
+   } else { // else bl
+    status = "Not eligible."; // execute
+}
+
+//Data types
+var age = 18; // number
+var name = "Jane"; // string
+var name = {first:"Jane", last:"Doe"}; // object
+var truth = false; // boolean
+var sheets = ["HTML","CSS","JS"]; // array
+var a; typeof a; // undefined
+var a = null; 
