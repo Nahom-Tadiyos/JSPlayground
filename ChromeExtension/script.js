69b456e54c2976ab119fde9aa1d37990cdf97623
Nahom <8nahom8@gmail.com>
2025-04-26 12:12:32 -0500

    switched to an event listener

--- ChromeExtension/script.js
+++ ChromeExtension/script.js
@@ -1,3 +1,9 @@
 function saveLead(){
     console.log("Click")
 }
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function(){
+    console.log("Button Clicked from addEventListener")
+})
