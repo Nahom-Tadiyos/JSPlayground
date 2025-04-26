9e6f956fcdd9cc7337bc2f079c37cf82497c82d9
Nahom <8nahom8@gmail.com>
2025-04-25 21:00:54 -0500

    added arrays

--- CodeWithMosh/script.js
+++ CodeWithMosh/script.js
@@ -41,3 +41,14 @@ var truth = false; // boolean
 var sheets = ["HTML","CSS","JS"]; // array
 var a; typeof a; // undefined
 var a = null; 
+
+//Arrays
+var dogs = ["Bulldog", "Beagle", "Labrador"]; 
+var dogs = new Array("Bulldog", "Beagle", "Labrador");  // declaration
+
+alert(dogs[1]);             // access value at index, first item being [0]
+dogs[0] = "Bull Terier";    // change the first item
+
+for (var i = 0; i < dogs.length; i++) {     // parsing with array.length
+console.log(dogs[i]);
+}
